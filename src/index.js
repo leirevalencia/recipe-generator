f415b19a58b2fc51ab460ebf29e69cb140651715
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2024-02-16 21:22:15 +0100

    javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Welcome!!🤗");
+function generateRecipe(event) {
+    event.preventDefault();
+
+    new Typewriter('#recipe', {
+        strings: "the recipe will go here",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+}
+
+let recipeElement = document.querySelector("#recipe-generator");
+recipeElement.addEventListener ('submit', generateRecipe);
