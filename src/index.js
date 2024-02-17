1b98e14d70309bf34fce74334a4842374f05402b
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2024-02-17 21:47:43 +0100

    recipe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function displayRecipe(response) {
+  console.log("here is your recipe");
+  new Typewriter('#recipe', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+ 
+
 function generateRecipe(event) {
     event.preventDefault();
 
-    new Typewriter('#recipe', {
-        strings: "the recipe will go here",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
+    let instructionInput = document.querySelector("#user-instructions");
+    let apiKey = "d48505t8703c543fo79ae4f4d413e7bf";
+    let prompt = `Generate quick and healthy vegetarian recipes about ${instructionInput}`;
+    let context = `Create a quick recipe with the ingredient given to you with basic ingredients you 
+    can find at home; separate the ingredients and the instructions so it is easy to read`;
+
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeElement = document.querySelector("#recipe-generator");
